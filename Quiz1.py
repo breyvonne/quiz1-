54ee715b81b8e5cb98fc661789f9157bf6284540
Breanna <123026437+breyvonne@users.noreply.github.com>
2023-01-27 11:45:05 -0500

    Add files via upload

---
+++ Quiz1.py
@@ -0,0 +1,15 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+2+(5-3)**5*27/(4*3)
+74.0
+print ('Be not afraid of greatness \n some are born great,some achieve greatness, and otoer
+       
+SyntaxError: unterminated string literal (detected at line 1)
+print ('Be not afraid of greatness \n some are born great,some achieve greatness, and ')
+       
+Be not afraid of greatness 
+ some are born great,some achieve greatness, and 
+print ('Be not afraid of greatness \n some are born great,some achieve greatness, and others have greatness thrust upon them')
+       
+Be not afraid of greatness 
+ some are born great,some achieve greatness, and others have greatness thrust upon them
